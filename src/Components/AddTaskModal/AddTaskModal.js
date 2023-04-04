ce7dd592063046e9665b67bb2ccb0eb80b2f269d
Mezan2002 <mezanurr009@gmail.com>
2023-04-04 23:25:07 +0600

    Add task modal design done and handle submit is working

--- src/Components/AddTaskModal/AddTaskModal.js
+++ src/Components/AddTaskModal/AddTaskModal.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 const Modal = () => {
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const title = form.title.value;
+    const description = form.description.value;
+    const date = form.date.value;
+    const taskData = {
+      title,
+      description,
+      date,
+    };
+  };
   return (
     <div>
       {/* The button to open modal */}
       <label htmlFor="addTaskModal" className="btn btn-primary btn-block">
-        <button className=""> Add New Task </button>
+        Add New Task
       </label>
 
       {/* Put this part before </body> tag */}
@@ -18,13 +30,46 @@ const Modal = () => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <h3 className="text-lg font-bold">Add a task</h3>
+          <form onSubmit={handleAddTask} className="mt-5">
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Title of the task</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Title"
+                name="title"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Description of the task</span>
+              </label>
+              <input
+                type="text"
+                name="description"
+                placeholder="Description"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Date of the task</span>
+              </label>
+              <input
+                type="text"
+                name="date"
+                placeholder="example: 04/04/2023"
+                className="input input-bordered w-full"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-block mt-5">
+              {" "}
+              Add Task{" "}
+            </button>
+          </form>
         </div>
       </div>
     </div>
