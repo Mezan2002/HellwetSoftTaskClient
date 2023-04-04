ef83c2985696e128073efbf887c535ea84c08eb8
Mezan2002 <mezanurr009@gmail.com>
2023-04-04 15:51:30 +0600

    New task add modal and many dashboard icons and many more things dome

--- src/Components/DashboardMenu/DashboardMenu.js
+++ src/Components/DashboardMenu/DashboardMenu.js
@@ -4,231 +4,133 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const DashboardMenu = () => {
   return (
-    <div className="min-h-screen relative fixed">
-      <div>
-        <p className="pt-4 pb-2 font-semibold text-4xl text-center px-2">
-          Dashboard <span className="text-xs">Demo</span>
-        </p>
-      </div>
-      <div className="px-2">
-        <ul className="menu bg-base-100 p-2 rounded-box">
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-              All Tasks
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Completed Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Incompleted Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Important Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Personal Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Team Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Proceessed Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Bookmarked Task
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Folder
-            </a>
-          </li>
-          <li className="my-1">
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
-              </svg>
-              Tags
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="px-4">
-        <ProCard></ProCard>
-      </div>
-      <div className="flex items-center justify-between mainBG absolute bottom-0 w-full py-6 px-3">
-        <div className="flex items-center">
-          <div className="avatar  mr-2">
-            <div className="w-12 rounded-full">
+    <div className="">
+      <div className="min-h-screen relative">
+        <div className="">
+          <p className="pt-4 pb-2 font-semibold text-4xl text-center px-2">
+            Dashboard <span className="text-xs">Demo</span>
+          </p>
+        </div>
+        <div className="px-2">
+          <div className="menu bg-base-100 p-2 rounded-box">
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
               <img
-                src="https://cdn.britannica.com/51/182851-050-97EA5117/Publicity-image-Iron-Man-War-Machine-2.jpg"
-                alt="loggedInUserImage"
+                draggable={false}
+                src="https://i.ibb.co/F32zxjZ/action.png"
+                alt=""
+                className="w-6 mr-5"
               />
+              <p className="">All Task</p>
             </div>
-          </div>
-          <div>
-            <div>
-              <h2 className="mr-5 font-bold">Iron Man</h2>
-              <p className="text-gray-400">user</p>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/TY9wBs9/task-actions.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Completed Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/zZ5XPy3/task.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Incompleted Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/RSnNj91/clipboard.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Important Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/6BywRwr/user-3.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Personal Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/SRt8KrS/community.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Team Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/Yh5x0Rw/work-time.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Proccessed Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/1TBPTLM/bookmark.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Bookmarked Task</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/KWWHzjh/folder.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Folder</p>
+            </div>
+            <div className="flex items-center hover:bg-gray-200 2xl:p-3 2xl:m-1 lg:p-3 lg:m-1 rounded-2xl">
+              <img
+                draggable={false}
+                src="https://i.ibb.co/9bhWHcD/tag-1.png"
+                alt=""
+                className="w-6 mr-5"
+              />
+              <p className="">Tags</p>
             </div>
           </div>
         </div>
-        <div>
-          <span>
-            {" "}
-            <IoIosArrowDown></IoIosArrowDown>{" "}
-          </span>
+        <div className="px-4">
+          <ProCard></ProCard>
+        </div>
+        <div className="flex items-center justify-between mainBG absolute bottom-0 w-full py-6 px-3">
+          <div className="flex items-center">
+            <div className="avatar  mr-2">
+              <div className="w-12 rounded-full">
+                <img
+                  src="https://cdn.britannica.com/51/182851-050-97EA5117/Publicity-image-Iron-Man-War-Machine-2.jpg"
+                  alt="loggedInUserImage"
+                />
+              </div>
+            </div>
+            <div>
+              <div>
+                <h2 className="mr-5 font-bold">Iron Man</h2>
+                <p className="text-gray-400">user</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span>
+              {" "}
+              <IoIosArrowDown></IoIosArrowDown>{" "}
+            </span>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -2,10 +2,11 @@ import React from "react";
 import { FaRegChartBar, FaSearch } from "react-icons/fa";
 import { MdNotificationsActive } from "react-icons/md";
 import { RiSettings4Fill } from "react-icons/ri";
+import AddTaskModal from "../AddTaskModal/AddTaskModal";
 
 const Tasks = () => {
   return (
-    <div className="min-h-screen mainBG">
+    <div className="mainBG min-h-screen">
       <div className="grid grid-cols-9 p-8">
         <div className="col-span-8 flex items-center relative">
           <p className="font-semibold text-2xl">Hello Iron👋 , </p>
@@ -31,7 +32,7 @@ const Tasks = () => {
         </div>
       </div>
       <div className="px-8">
-        <div className="card card-compact bg-base-100 shadow-xl">
+        <div className="bg-base-100 shadow-xl h-[730px] card card-compact">
           <div className="p-4 flex items-center justify-between">
             <div className="w-8/12">
               <h2 className="card-title">All Tasks</h2>
@@ -49,128 +50,141 @@ const Tasks = () => {
                 </p>
               </div>
               <div className="">
-                <button className="btn btn-primary"> Add new task </button>
+                <AddTaskModal></AddTaskModal>
               </div>
             </div>
           </div>
-        </div>
-        <div className="mt-5 h-96">
-          <div className="overflow-x-auto">
-            <table className="table rounded-full max-h-[30%] w-full">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Title</th>
-                  <th>Description</th>
-                  <th>Date</th>
-                  <th>Status</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* row 1 */}
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>Completed</td>
-                  <td className="">
-                    <button className="btn btn-sm mr-4">edit</button>
-                    <button className="btn btn-sm">delete</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="h-[80%] overflow-auto">
+            <div className="mt-5">
+              <div className="overflow-x-auto">
+                <table className="table rounded-none w-full">
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Title</th>
+                      <th>Description</th>
+                      <th>Date</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* row 1 */}
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>1</th>
+                      <td>Cy Ganderton</td>
+                      <td>Quality Control Specialist</td>
+                      <td>Blue</td>
+                      <td>Completed</td>
+                      <td className="">
+                        <button className="btn btn-sm mr-4">edit</button>
+                        <button className="btn btn-sm">delete</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
